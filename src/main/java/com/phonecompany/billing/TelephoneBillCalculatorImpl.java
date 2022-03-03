2014e636a15f244e662459a8dc3262a2c5801380
Patrik Hanes <patrik@hanes.sk>
2022-03-03 23:48:02 +0100

    Partially working build.

--- src/main/java/com/phonecompany/billing/TelephoneBillCalculatorImpl.java
+++ src/main/java/com/phonecompany/billing/TelephoneBillCalculatorImpl.java
@@ -1,26 +1,31 @@
 package com.phonecompany.billing;
 
-import java.math.BigInteger;
-import java.time.Duration;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.math.BigDecimal;
 import java.util.stream.Collectors;
 
 public class TelephoneBillCalculatorImpl implements TelephoneBillCalculator {
 
-    public static final int YEAR_SECONDS = 31536000;
     public static final int DAY_SECONDS = 86400;
-
     public static final int HOUR_SECONDS = 3600;
     public static final int MINUTE_SECONDS = 60;
 
 
-    public record Time (int hour, int minute, int sec) {}
+    public record Time (int hour, int minute, int sec) {
+        public String getFormat () {
+
+            String hourString = String.format("%02d", hour);
+            String minuteString = String.format("%02d", minute);
+            String secString = String.format("%02d", sec);
+
+            return hourString + " " + minuteString + " " + secString;
+        }
+    }
 
     public record Date (int day, int month, int year) {
         public String getFormat () {
@@ -61,7 +66,6 @@ public class TelephoneBillCalculatorImpl implements TelephoneBillCalculator {
 
         return result;
     }
-
 
 
     public long getSecondsFromTimestamps (TimeStamp phoneStart, TimeStamp phoneEnd) {
@@ -120,21 +124,105 @@ public class TelephoneBillCalculatorImpl implements TelephoneBillCalculator {
         return count;
     }
 
-    public String getMaxCall ( List<PhoneLog> phoneLogs ) {
-        List<String>phoneNumbers = new ArrayList<>();
-        phoneLogs.forEach(phoneLog -> phoneNumbers.add(phoneLog.phoneNumber));
 
+    public String getSpecialPhoneNumber ( List<PhoneLog> phoneLogs ) {
+
+        // Create an array that will hold all the records of the phone numbers called
+        List<String>phoneNumberRecords = new ArrayList<>();
+        phoneLogs.forEach(phoneLog -> phoneNumberRecords.add(phoneLog.phoneNumber));
+
+        // Create an array with only the unique phone numbers (remove duplicates from records)
+        List<String> phoneNumbersDistinct = phoneNumberRecords.stream().distinct().collect(Collectors.toList());
+
+        // Create a hash map with the key being the phone number, and the value being the occurrence count
         HashMap<String, Integer>phoneNumberCount = new HashMap<>();
-        List<String> phoneNumbersDistinct = phoneNumbers.stream().distinct().collect(Collectors.toList());
-
         for (var phoneNumber: phoneNumbersDistinct)
-            phoneNumberCount.put(phoneNumber, getPhoneNumberCount(phoneNumbers, phoneNumber));
+            phoneNumberCount.put(phoneNumber, getPhoneNumberCount(phoneNumberRecords, phoneNumber));
 
+        // Based on the number of occurrences, filter the array to only the phone numbers with the most occurrences (max)
+        int maxCount = phoneNumberCount.entrySet().stream().max(Comparator.comparingInt(Map.Entry::getValue)).get().getValue();
+        Map<String, Integer> filteredPhoneNumberCount = phoneNumberCount.entrySet().stream().filter(entry -> entry.getValue() == maxCount).collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue()));
 
+        if (phoneNumberCount.size() == 1) // Only one max value
+            return phoneNumberCount.keySet().stream().toList().get(0);
 
-        return "";
+        else { // Multiple max values
+
+            // Replace occurrences with arithmetic values
+            filteredPhoneNumberCount = filteredPhoneNumberCount.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, entry -> getArithmeticValueOfPhoneNumber(entry.getKey())));
+
+            // Get the result phone number based on the max arithmetic value
+            String resultPhoneNumber = filteredPhoneNumberCount.entrySet().stream().max(Comparator.comparingInt(Map.Entry::getValue)).get().getKey();
+            return resultPhoneNumber;
+        }
     }
 
+    private Integer getArithmeticValueOfPhoneNumber(String phoneNumber) {
+        int length = phoneNumber.length();
+        int sum = 0;
+        for (int i = 0; i < length; i++)
+            sum += Character.getNumericValue(phoneNumber.charAt(i));
+        return sum;
+    }
+
+
+    public double firstCondition ( long seconds, TimeStamp timeStamp ) throws ParseException {
+        String leftBound = "07 55 00";
+        String rightBound = "15 59 59";
+
+        String leftEdge = "08 00 00";
+        String rightEdge = "15 54 59";
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH mm ss");
+
+        java.util.Date startDate = dateFormat.parse(timeStamp.time.getFormat());
+        java.util.Date leftBoundDate = dateFormat.parse(leftBound);
+        java.util.Date rightBoundDate = dateFormat.parse(rightBound);
+
+        long leftBoundDifMS = startDate.getTime() - leftBoundDate.getTime();
+        long rightBoundDifMS = startDate.getTime() - rightBoundDate.getTime();
+
+        if ( (leftBoundDifMS <= 0) || (rightBoundDifMS >= 0) ) // Outside interval (07:55:00 || 15:59:59)
+            return Math.ceil(seconds / 60) * 0.50;
+
+
+        java.util.Date leftEdgeDate = dateFormat.parse(leftEdge);
+        java.util.Date rightEdgeDate = dateFormat.parse(rightEdge);
+        long leftEdgeDifMS = startDate.getTime() - leftEdgeDate.getTime();
+        long rightEdgeDifMS = startDate.getTime() - rightEdgeDate.getTime();
+
+        if (leftEdgeDifMS < 0) { // To the left of 08:00:00
+            long differenceInSeconds = (Math.abs(leftEdgeDifMS) / 1000) % 60;
+            if (differenceInSeconds > seconds)
+                return (differenceInSeconds * 0.50);
+            else
+                return (differenceInSeconds * 0.50) +  ((seconds - differenceInSeconds) * 0.20);
+        }
+        else if (rightEdgeDifMS > 0) { // To the right of 15:54:59
+            long differenceInSeconds = (Math.abs(rightEdgeDifMS) / 1000) % 60;
+            if (differenceInSeconds > seconds)
+                return (differenceInSeconds * 0.20);
+            else
+                return (differenceInSeconds * 0.20) +  ((seconds - differenceInSeconds) * 0.50);
+        }
+        else { // Inside the interval
+            return Math.ceil(seconds / 60) * 0.20;
+        }
+    }
+
+    public double secondCondition ( long seconds, TimeStamp timeStamp ) throws ParseException {
+        if (seconds - (5 * 60) <= 0)
+            return firstCondition(seconds, timeStamp);
+        else {
+            double result = Math.ceil((seconds - (5 * 60)) / 60);
+            return result + firstCondition(5 * 60, timeStamp);
+        }
+    }
+
+    public List<PhoneLog> removeSpecialPhoneNumber(List<PhoneLog> phoneLogs) {
+        String specialPhoneNumber = getSpecialPhoneNumber(phoneLogs);
+        return phoneLogs.stream().filter(phoneLog -> !phoneLog.phoneNumber.equals(specialPhoneNumber)).toList();
+    }
 
     @Override
     public BigDecimal calculate(String phoneLog) {
@@ -146,8 +234,17 @@ public class TelephoneBillCalculatorImpl implements TelephoneBillCalculator {
             phoneLogs.add(getPhoneLog(phoneCall));
         }
 
-        long result = getSecondsFromTimestamps(phoneLogs.get(0).phoneStart, phoneLogs.get(0).phoneEnd );
+        // Remove special number from calls
+        // return the result of calculation of the first and second condition
+        phoneLogs = removeSpecialPhoneNumber(phoneLogs);
+        double result = 0;
+        for (PhoneLog log: phoneLogs) {
+            try {
+                result += secondCondition(getSecondsFromTimestamps(log.phoneStart, log.phoneEnd), log.phoneStart());
+            } catch (ParseException ignored) {
+            }
 
+        }
         return new BigDecimal(0);
     }
 }
